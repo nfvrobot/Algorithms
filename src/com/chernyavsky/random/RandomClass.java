856e4920c8651e238bdeb0b5439a9b3c6f49bbe3
nfvrobot <fraggedbynfv@ggmail.com>
2021-01-04 19:04:41 +0300

    message

---
+++ src/com/chernyavsky/random/RandomClass.java
@@ -0,0 +1,9 @@
+package com.chernyavsky.random;
+
+public class RandomClass {
+
+    public static void main(String[] args) {
+        System.out.println("1231231");
+        Object data = new Object();
+    }
+}
